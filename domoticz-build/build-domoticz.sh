60bf963828f252d7c01447844e601b8579781beb
Daniel Perez <daniel.horacio@gmail.com>
2020-04-18 18:54:10 +0200

    initial commit

---
+++ domoticz-build/build-domoticz.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+mkdir -p /opt/domoticz
+cd /opt/
+wget https://github.com/Kitware/CMake/releases/download/v$CMAKE_VERSION/cmake-$CMAKE_VERSION.tar.gz
+tar -xzvf cmake-${CMAKE_VERSION}.tar.gz
+rm cmake-${CMAKE_VERSION}.tar.gz
+cd /opt/cmake-${CMAKE_VERSION}
+./bootstrap
+make -j 3
+make install
+rm -Rf /opt/cmake-${CMAKE_VERSION}
+cd /opt/
+git clone https://github.com/OpenZWave/open-zwave.git
+ln -s open-zwave open-zwave-read-only
+cd /opt/open-zwave
+git checkout ${OPENZWAVE_VERSION_TAG}
+make -j 3
+cd /opt/
+git clone https://github.com/domoticz/domoticz.git
+cd /opt/domoticz
+git checkout ${DOMOTICZ_VERSION_TAG}
+cd /opt/domoticz
+cmake -DCMAKE_BUILD_TYPE=Release CMakeLists.txt
+make -j 3
+
+mkdir -p /opt/domoticz/conf
+rm -rf /opt/domoticz/.git
+cd /opt/
+tar -cvzf /output/domoticz-${DOMOTICZ_VERSION_TAG}.tar.gz ./domoticz
